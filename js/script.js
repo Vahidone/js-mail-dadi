6dc3e093569dd8c79e06bb7cf00f93753f881879
Vahidone <v.ban.roma@gmail.com>
2023-09-12 19:07:21 +0200

    feat: add feature in js for gioco dadi

--- js/script.js
+++ js/script.js
@@ -5,6 +5,7 @@
 // creare una variabile della lista delle email presenti 
 
 const autorizzateEmails= ['ospite1@esempio.com', 'ospite2@esempio.com', 'ospite3@esempio.com', 'ospite4@esempio.com', 'ospite5@esempio.com' ];
+
 let message;
 // decidere dove va stampato il messaggio 
 const printMessage = document.getElementById('print-message');
@@ -40,6 +41,52 @@ btnReset.addEventListener('click', function () {
 });
 
 
+
+
+
+
 // ---------------GIOCO DI DADI--------------------
 
+let message2;
+const bottonePlay = document.getElementById('btn-play');
+const result = document.getElementById('risultato');
+// dare una funzione al bottone GIOCA 
 
+ bottonePlay.addEventListener('click', function () {
+  let playerPoint = 0;
+  let computerPoint = 0;
+  let playerNumber = Math.floor(Math.random() * 6) + 1;
+  let computerNumber = Math.floor(Math.random() * 6) + 1;
+  playerPoint += playerNumber;
+  computerPoint += computerNumber;
+
+ 
+
+  if (playerPoint > computerPoint) {
+    message2 = 'Hai vinto tu';
+
+  }else if (playerPoint < computerPoint ) {
+    message2 = 'Ha vinto il computer';
+  }else {
+    message2 = 'hai pareggiato';
+  }
+  
+  result.innerHTML = message2;
+  
+ });
+
+
+//  dare una funzione reset al bottone Annulla 
+
+const bottoneAnnulla = document.getElementById('btn-annulla');
+
+bottoneAnnulla.addEventListener('click', function() {
+  result.classList.add('d-none');
+  location.reload();
+});
+
+
+
+
+ 
+ 
