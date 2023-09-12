14cb30a061e07490b9ba759ca2dc7f96894879b7
Vahidone <v.ban.roma@gmail.com>
2023-09-12 16:36:46 +0200

    feat: made js code for mail exercise

---
+++ js/script.js
@@ -0,0 +1,37 @@
+// creare una variabile della lista delle email presenti 
+
+const autorizzateEmails= ['ospite1@esempio.com', 'ospite2@esempio.com', 'ospite3@esempio.com', 'ospite4@esempio.com', 'ospite5@esempio.com' ];
+let message;
+// decidere dove va stampato il messaggio 
+const printMessage = document.getElementById('print-message');
+
+// il bottone di verifica 
+const btnValidation = document.getElementById('button-verifica')
+// il bottone di reset 
+btnReset = document.getElementById('button-reset');
+
+btnValidation.addEventListener('click', function () {
+  // chiedere all'utente di inserire la sua email 
+  const inputEmail = document.getElementById('input-email').value;
+
+  // operazione di CONTROLLO E STAMPA
+
+if (autorizzateEmails.includes(inputEmail)) {
+  message = 'Accesso consentito. Benvenuto!';
+  printMessage.append(message);
+}
+else {
+  message = 'Accesso negato. Indirizzo email non è autorizzato';
+  printMessage.append(message);
+  console.log(message);
+}
+
+});
+
+// operazione di  RESET 
+btnReset.addEventListener('click', function () {
+  printMessage.classList.add('d-none');
+  location.reload()
+});
+
+
